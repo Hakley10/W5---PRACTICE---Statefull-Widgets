35fb153500dfb165f7e435851fc07f7ddafd9b0e
Heng Hakley <Hakleybtb18kh@gmail.com>
2025-11-20 03:05:44 +0700

    done

--- lib/EX-3/ex_3_start.dart
+++ lib/EX-3/ex_3_start.dart
@@ -1,129 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ScoreBarApp());
+List<String> images = [
+  "assets/w4-s2/bird.jpg",
+  "assets/w4-s2/bird2.jpg",
+  "assets/w4-s2/insect.jpg",
+  "assets/w4-s2/girl.jpg",
+  "assets/w4-s2/man.jpg",
+];
 
-class ScoreBarApp extends StatelessWidget {
-  const ScoreBarApp({super.key});
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false, // Hides the DEBUG banner in debug mode
+      home: ImageGallery(),
+    ));
+
+class ImageGallery extends StatefulWidget {
+  const ImageGallery({super.key});
+
+  @override
+  State<ImageGallery> createState() => _ImageGalleryState();
+}
+
+class _ImageGalleryState extends State<ImageGallery> {
+
+  int _currentImageIndex = 0;
+
+
+  void _goToPreviousImage() {
+    setState(() {
+      if (_currentImageIndex > 0) {
+        _currentImageIndex--;
+      } else {
+
+        _currentImageIndex = images.length - 1;
+      }
+    });
+  }
+
+  void _goToNextImage() {
+    setState(() {
+      if (_currentImageIndex < images.length - 1) {
+        _currentImageIndex++;
+      } else {
+
+        _currentImageIndex = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Score Rating System')),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              ScoreCard(
-                title: 'My score in Flutter',
-              ),
-              SizedBox(height: 16),
-              ScoreCard(
-                title: 'My score in Dart',
-              ),
-              SizedBox(height: 16),
-              ScoreCard(
-                title: 'My score in React',
-              ),
-            ],
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: _goToPreviousImage,
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class ScoreCard extends StatefulWidget {
-  final String title;
-
-  const ScoreCard({
-    super.key,
-    required this.title,
-  });
-
-  @override
-  State<ScoreCard> createState() => _ScoreCardState();
-}
-
-class _ScoreCardState extends State<ScoreCard> {
-  int _currentScore = 5;
-
-  void _incrementScore() {
-    setState(() {
-      if (_currentScore < 10) {
-        _currentScore++;
-      }
-    });
-  }
-
-  void _decrementScore() {
-    setState(() {
-      if (_currentScore > 1) {
-        _currentScore--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Title
-            Text(
-              widget.title,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: _goToNextImage,
             ),
-            const SizedBox(height: 16),
-            
-            // Progress Bar Container with +/- buttons inside
-            Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(color: Colors.grey[400]!),
-              ),
-              child: Row(
-                children: [
-                  // Minus button on the left
-                  IconButton(
-                    onPressed: _decrementScore,
-                    icon: const Icon(Icons.remove),
-                  ),
-                  
-                  // Expanded space in the middle
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        '$_currentScore',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  
-                  // Plus button on the right
-                  IconButton(
-                    onPressed: _incrementScore,
-                    icon: const Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
+      body:Image.asset(images[_currentImageIndex]),
+  
     );
   }
 }
